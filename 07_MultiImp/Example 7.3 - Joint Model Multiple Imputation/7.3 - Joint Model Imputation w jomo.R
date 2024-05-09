adfa47f6efa0b58ce330673aec73c795770b1e9b
hanadate <s2421310@u.tsukuba.ac.jp>
2024-05-10 00:12:00 +0900

    Update 7.3 - Joint Model Imputation w jomo.R

--- 07_MultiImp/Example 7.3 - Joint Model Multiple Imputation/7.3 - Joint Model Imputation w jomo.R
+++ 07_MultiImp/Example 7.3 - Joint Model Multiple Imputation/7.3 - Joint Model Imputation w jomo.R
@@ -1,5 +1,8 @@
 # example 7.3: joint model multiple imputation w jomo
 # requires fdir and jomo packages
+library(fdir)
+library(jomo)
+library(tidyverse)
 
 # set working directory
 fdir::set()
@@ -7,6 +10,9 @@ fdir::set()
 # read data from working directory
 dat <- read.table("math.dat", na.strings = "999")
 names(dat) <- c("id", "male", "frlunch", "achievegrp", "stanread", "efficacy", "anxiety", "mathpre", "mathpost")
+
+# inspect dat
+summary(dat)
 
 # define binary variable as factor
 dat$frlunch <- factor(data$frlunch, levels = c(0,1))
