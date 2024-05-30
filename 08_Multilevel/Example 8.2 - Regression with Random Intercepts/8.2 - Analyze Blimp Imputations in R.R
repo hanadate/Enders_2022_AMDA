7fd7caf22d39b8986fa72c698342636c42f38373
hanadate <s2421310@u.tsukuba.ac.jp>
2024-05-30 09:31:54 +0900

    Update 8.2 - Analyze Blimp Imputations in R.R

--- 08_Multilevel/Example 8.2 - Regression with Random Intercepts/8.2 - Analyze Blimp Imputations in R.R
+++ 08_Multilevel/Example 8.2 - Regression with Random Intercepts/8.2 - Analyze Blimp Imputations in R.R
@@ -1,15 +1,27 @@
 # example 8.2: analyze model-based multiple imputations for a multilevel regression with random intercepts
 # requires fdir, rockchalk, lme4, and mitml packages
+library(fdir)
+library(rockchalk)
+library(lme4)
+library(mitml)
+library(tidyverse)
 
 # set working directory
 fdir::set()
+
+# inspect problemsolving2level.dat
+edu2lv <- read_table("problemsolving2level.dat",col_names = FALSE) 
+dim(edu2lv) # 982 14
+names(edu2lv) <- c("school","student","condition","teachexp","eslpct","ethnic","male","frlunch",
+                   "achievegrp","stanmath","efficacy1","efficacy2","probsolve1","probsolve2") 
+
 
 # read imputed data from working directory
 imps <- read.table("imps.dat")
 names(imps) <- c("Imputation","school","student","condition","teachexp","eslpct","ethnic","male","frlunch",
                  "achievegrp","stanmath","efficacy1","efficacy2","probsolve1","probsolve2",
                  "ranicept.l2", "frlunch.latent", "frlunch.l2mean","stanmath.l2mean", "probsolv1.l2mean") 
-
+summary(imps)
 # distribution of random intercept residuals
 plot(density(imps$ranicept.l2))
 rockchalk::skewness(imps$ranicept.l2)
